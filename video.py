b7912144843a9e0e5525faf899eeba1110c176f5
Scott Fitsimones <iscottonline@gmail.com>
2017-05-30 13:08:49 -0700

    classify face

--- video.py
+++ video.py
@@ -22,17 +22,18 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     for (x,y,w,h) in faces:
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = image[y:y+h, x:x+w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        eye1 = eyes[0]
-        eye2 = eyes[1]
-        avgx = (eye1[0] + eye2[0])/2
-        avgy = (eye1[1] + eye2[1])/2
-        avgw = (eye1[2] + eye2[2])/2
-        min_x = min(eye1[0], eye2[0])
-        max_x = max(eye1[0], eye2[0])
-        min_y = min(eye1[0], eye2[0])
-        max_y = max(eye1[0], eye2[0])
-        cv2.rectangle(roi_color,(min_x,min_y+30),(max_x+avgw, max_y-30),(0,255,0),2)
+        # eyes = eye_cascade.detectMultiScale(roi_gray)
+        # eye1 = eyes[0]
+        # eye2 = eyes[1]
+        # avgx = (eye1[0] + eye2[0])/2
+        # avgy = (eye1[1] + eye2[1])/2
+        # avgw = (eye1[2] + eye2[2])/2
+        # min_x = min(eye1[0], eye2[0])
+        # max_x = max(eye1[0], eye2[0])
+        # min_y = min(eye1[0], eye2[0])
+        # max_y = max(eye1[0], eye2[0])
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        # cv2.rectangle(roi_color,(min_x,min_y+30),(max_x+avgw, max_y-30),(0,255,0),2)
 
     cv2.imshow("Frame", image)
     key = cv2.waitKey(1) & 0xFF
